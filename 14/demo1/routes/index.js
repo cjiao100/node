ea99203ca2db44dee8117bedc05cac25d28ca929
cjiao100 <cjiao100@163.com>
2019-05-22 22:15:50 +0800

    5.22

--- 14/demo1/routes/index.js
+++ 14/demo1/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express',
+        msg: '这是一条信息',
+        arr: ['a', 'b', 'c']
+    });
 });
 
 module.exports = router;
